517880fe95154bb1628e9814a555cbe9f148b05a
Marcela Malaver <marcela9409@gmail.com>
2020-07-25 12:57:33 -0500

    crear customHook para accion de cargando...

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,25 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import { GifGridItem } from './GifGridItem';
 import { getGifs } from '../helpers/getGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({category}) => {
-    const [images, setImages] = useState([]);
+    /* const [images, setImages] = useState([]);
     useEffect(() => {
         //getGifs(category).then(imagenes=>setImages(imagenes))
         getGifs(category).then(setImages)
-    }, [category])
+    }, [category]) */
 
+    const {loading} = useFetchGifs();
+    console.log(loading);
     return (
         <>
             <h3>{category}</h3>
+            {loading ? 'Loading...': 'Data cargada'}
             
-            <div className="w3-row-padding">
+            {/* <div className="w3-row-padding">
                 {
                     images.map((img) => (
                         <GifGridItem key={img.id} {...img}/>
                     ))    
                 }
-            </div>
+            </div> */}
         </>
     )
 }
