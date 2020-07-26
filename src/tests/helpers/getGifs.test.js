9439efa9ce10855784641afe09dc2d9c64ae6013
Marcela Malaver <marcela9409@gmail.com>
2020-07-26 16:48:44 -0500

    crear set de pruebas sobre helper GetGifs

---
+++ src/tests/helpers/getGifs.test.js
@@ -0,0 +1,18 @@
+import {getGifs} from '../../helpers/getGifs';
+
+
+describe('Pruebas en helper getGifs (fetch)',()=>{
+
+    test('debe de traer 9 elementos', async() => {     
+        const gifs = await getGifs('ferdinand');
+        expect(gifs.length).toBe(9);
+    })
+
+    test('debe de traer 0 elementos', async() => {     
+        const gifs = await getGifs('');
+        expect(gifs.length).toBe(0);
+    })
+
+});
+
+
